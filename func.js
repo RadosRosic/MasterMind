d5d4024e50911dc22a1be401895ed0f972d1b80b
RadosRosic <rados.rosic@gmail.com>
2022-12-24 12:57:21 +0100

    first public version

---
+++ func.js
@@ -0,0 +1,112 @@
+const btns = Array.from(document.getElementsByClassName("btn"));
+const btnDel = document.getElementById("btnDel");
+const btnTry = document.getElementById("btnTry");
+const currentRoundText = document.getElementById("current-round");
+const main = document.getElementById("main");
+
+btns.forEach((e) => {
+  e.addEventListener("click", select);
+});
+btnDel.addEventListener("click", del);
+btnTry.addEventListener("click", guess);
+
+let round = 1;
+let won = false;
+currentRoundText.textContent = round;
+
+const combination = [];
+let first, second, third, fourth;
+first = randomNumber();
+second = randomNumber();
+third = randomNumber();
+fourth = randomNumber();
+combination.push(first, second, third, fourth);
+
+const playerGuess = [];
+const result = [];
+
+function randomNumber() {
+  return Math.trunc(Math.random() * 6) + 1;
+}
+
+console.log(combination);
+
+function guess() {
+  const activeBox = document.getElementById(`hintBox${round}`);
+  console.log(combination, playerGuess);
+
+  if (playerGuess.length === 4) {
+    if (playerGuess.join("") === combination.join("")) {
+      console.log("WON");
+      currentRoundText.textContent = "WON";
+      main.style.backgroundColor = "aqua";
+      return;
+    }
+
+    const combinationCopy = [...combination]; // make a true copy of combination array, needed for splicing
+    for (let i = 0; i < playerGuess.length; i++) {
+      if (playerGuess[i] === combinationCopy[i]) {
+        result.push("!");
+        combinationCopy.splice(i, 1, 0); // replace el with 0
+      }
+    }
+
+    for (let i = 0; i < playerGuess.length; i++) {
+      if (combinationCopy.includes(playerGuess[i])) {
+        result.push("?");
+        combinationCopy.splice(combinationCopy.indexOf(playerGuess[i]), 1, 0);
+      }
+    }
+
+    combinationCopy.forEach((el) => (el ? result.push("X") : null));
+
+    result.forEach((el) => {
+      const img = document.createElement("img");
+
+      if (el === "!") {
+        img.src = `./img/right.svg`;
+      }
+      if (el === "?") {
+        img.src = `./img/almost.svg`;
+      }
+      if (el === "X") {
+        img.src = `./img/wrong.svg`;
+      }
+      img.style.width = "65px";
+      img.style.margin = "0 12px";
+
+      activeBox.append(img);
+    });
+
+    // activeBox.textContent = result.join(" ");
+    result.length = 0;
+    playerGuess.length = 0;
+    round++;
+    if (round === 7) {
+      return;
+    }
+    currentRoundText.textContent = round;
+  }
+}
+
+function select(e) {
+  if (playerGuess.length < 4) {
+    const activeBox = document.getElementById(`guessBox${round}`);
+    const img = document.createElement("img");
+    img.style.width = "65px";
+    img.style.margin = "10px";
+
+    img.src = `./img/symbol${e.target.value}.svg`;
+    activeBox.append(img);
+    playerGuess.push(Number(e.target.value));
+  }
+}
+
+function del() {
+  const activeBox = document.getElementById(`guessBox${round}`);
+  if (playerGuess.length > 0) {
+    playerGuess.pop();
+    activeBox.removeChild(activeBox.lastChild);
+    // activeBox.textContent = activeBox.textContent.slice(0, -1);
+  }
+}
---
+++ script.js
@@ -0,0 +1,154 @@
+const root = document.body;
+
+const createElement = (el, parent, prepend = false) => {
+  const { nodeName = "div", ...attrs } = el;
+  const element = document.createElement(nodeName);
+  Object.entries(attrs).forEach(([attr, value]) => {
+    element[attr] = value;
+  });
+  if (prepend) parent.prepend(element);
+  else parent.append(element);
+};
+
+// game box
+createElement(
+  {
+    id: "playground",
+  },
+  root,
+  true
+);
+
+// header
+createElement(
+  {
+    nodeName: "header",
+    id: "header",
+  },
+  playground
+);
+
+createElement(
+  {
+    nodeName: "h1",
+    textContent: "Guessing Game",
+    id: "h1",
+  },
+  header
+);
+
+createElement(
+  {
+    nodeName: "h2",
+    id: "current-round",
+  },
+  header
+);
+
+// main
+createElement(
+  {
+    nodeName: "main",
+    id: "main",
+  },
+  playground
+);
+
+for (let i = 0; i < 2; i++) {
+  createElement(
+    {
+      nodeName: "section",
+      id: `section${i + 1}`,
+      classList: "section",
+    },
+    main
+  );
+}
+
+// display player guesses (left)
+for (let i = 0; i < 6; i++) {
+  createElement(
+    {
+      id: `guessBox${i + 1}`,
+      classList: `box`,
+    },
+    document.querySelector("#section1")
+  );
+}
+
+// display hints (right)
+for (let i = 0; i < 6; i++) {
+  createElement(
+    {
+      id: `hintBox${i + 1}`,
+      classList: `box`,
+    },
+    document.querySelector("#section2")
+  );
+}
+
+createElement(
+  {
+    nodeName: "section",
+    id: "buttons",
+  },
+  playground
+);
+
+createElement(
+  {
+    id: "guessAndDelBtns",
+  },
+  buttons
+);
+
+createElement(
+  {
+    nodeName: "button",
+    type: "button",
+    id: "btnTry",
+    textContent: "TRY",
+  },
+  guessAndDelBtns
+);
+
+createElement(
+  {
+    nodeName: "button",
+    type: "button",
+    id: "btnDel",
+    textContent: "DEL",
+  },
+  guessAndDelBtns
+);
+
+createElement(
+  {
+    id: "btnsSelectSection",
+  },
+  buttons
+);
+
+for (let i = 0; i < 6; i++) {
+  createElement(
+    {
+      nodeName: "img",
+      type: "image",
+      id: `btn${i}`,
+      src: `./img/symbol${i + 1}.svg`,
+      classList: "btn",
+      textContent: `${i + 1}`,
+      value: `${i + 1}`,
+    },
+    btnsSelectSection
+  );
+}
+
+createElement(
+  {
+    nodeName: "footer",
+    id: "footer",
+    textContent: "Rados Rosic 2022",
+  },
+  playground
+);
